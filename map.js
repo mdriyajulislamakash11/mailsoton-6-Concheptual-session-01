6bd733bc85ecb11aebd00c476c2e641a2067a6b3
Akash <hafejmohammadakash@gmail.com>
2025-04-03 00:39:16 -0700

    learn to map

---
+++ map.js
@@ -0,0 +1,22 @@
+
+// MAP
+
+
+const arr = [10, 20, 30, 40, 50];
+const result = arr.map((item) => {
+    item * 2  
+    return item
+})
+console.log("The items:", result);
+
+
+
+const arr = [10, 20, 30, 40, 50];
+const result = arr.map((item) => {
+  const add = item + 5;
+  return add;
+});
+console.log("the items:", result);
+
+
+
